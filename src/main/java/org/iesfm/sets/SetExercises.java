bd1d6cac94a87636053686982bcc3132373d90ab
Michatorski <MichalHomend@protonmail.com>
2021-03-23 11:41:46 +0100

    Ejercios

--- src/main/java/org/iesfm/sets/SetExercises.java
+++ src/main/java/org/iesfm/sets/SetExercises.java
@@ -1,6 +1,6 @@
 package org.iesfm.sets;
 
-import java.util.Set;
+import java.util.*;
 
 public class SetExercises {
     //set y un string, devuelve true o faLSE
@@ -8,6 +8,30 @@ public class SetExercises {
 
     return members.contains(member);
 
+    }
+
+    public Set<String> insertData(Set<String > setStringName, String stringName){
+         setStringName.add(stringName);
+         return setStringName;
+    }
+
+    public List<String> toList (Set<String> set){
+        List<String> list = new ArrayList<>(set);
+
+        return list;
+
+    }
+
+    public TreeSet<String > toSet (List<String> list){
+        Comparator <String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+        TreeSet<String> set = new TreeSet<>(comparator);
+        set.addAll(list);
+        return set;
     }
 
 }
--- src/test/java/org/iesfm/sets/SetexercisesTest.java
+++ src/test/java/org/iesfm/sets/SetexercisesTest.java
@@ -3,9 +3,7 @@ package org.iesfm.sets;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetexercisesTest {
 
@@ -29,5 +27,98 @@ public class SetexercisesTest {
 
         boolean res = setExercises.checkMember(members, "Raquel");
         Assert.assertFalse(res);
+    }
+
+    @Test
+    public void insertDataTest(){
+        Set<String> stringName = new HashSet<>();
+        stringName.add("Palabra");
+
+        setExercises.insertData(stringName, "Palabro");
+
+        Set<String> expected = new HashSet<>();
+        expected.add("Palabra");
+        expected.add("Palabro");
+
+        Assert.assertEquals(expected, stringName);
+
+    }
+
+    @Test
+    public void insertDataSameWordTest(){
+        Set<String> stringName = new HashSet<>();
+        stringName.add("Palabra");
+
+        setExercises.insertData(stringName, "Palabra");
+
+        Set<String> expected = new HashSet<>();
+        expected.add("Palabra");
+
+        Assert.assertEquals(expected, stringName);
+
+    }
+
+    @Test
+    public void toListTreeSetTest(){
+        setExercises = new SetExercises();
+
+        Set<String > tree = new TreeSet<>();
+        tree.add("A");
+        tree.add("C");
+        tree.add("B");
+
+        List<String > lists = setExercises.toList(tree);
+
+        List<String > expected = new ArrayList<>();
+        expected.add("A");
+        expected.add("B");
+        expected.add("C");
+
+        Assert.assertEquals(expected,lists);
+    }
+
+    @Test
+    public void toListHashSetTest(){
+        setExercises = new SetExercises();
+
+        Set<String > hash = new HashSet<>();
+        hash.add("AA");
+        hash.add("CC");
+        hash.add("BB");
+        hash.add("DD");
+        hash.add("EE");
+        hash.add("FF");
+
+        List<String > lists = setExercises.toList(hash);
+
+        List<String> expected = new ArrayList<>();
+        expected.add("AA");
+        expected.add("CC");
+        expected.add("BB");
+        expected.add("DD");
+        expected.add("EE");
+        expected.add("FF");
+
+        Assert.assertEquals(expected,lists);
+    }
+
+    @Test
+    public void toSetTest(){
+        setExercises = new SetExercises();
+
+        List<String > list = new ArrayList<>();
+        list.add("hola");
+        list.add("adios");
+        list.add("que");
+
+        TreeSet<String > set = setExercises.toSet(list);
+
+        List<String> expected = new ArrayList<>();
+        expected.add("que");
+        expected.add("hola");
+        expected.add("adios");
+
+
+        Assert.assertEquals(expected,setExercises.toList(set));
     }
 }
